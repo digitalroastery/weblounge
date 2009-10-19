cc8a45dc6c55df426830c43ea23cfaa5fc42889e
Tobias Wunden <tobias@entwinemedia.com>
2009-10-19 09:52:59 +0000

    Fixed license header

--- src/main/java/ch/o2it/weblounge/common/site/ActionConfiguration.java
+++ src/main/java/ch/o2it/weblounge/common/site/ActionConfiguration.java
@@ -1,14 +1,21 @@
-/**
- * ActionConfiguration.java
+/*
+ *  Weblounge: Web Content Management System
+ *  Copyright (c) 2009 The Weblounge Team
+ *  http://weblounge.o2it.ch
  *
- * Copyright 2003 by O2 IT Engineering
- * Zurich,  Switzerland (CH)
- * All rights reserved.
- * 
- * This software is confidential and proprietary information
- * ("Confidential Information"). You shall not disclose such
- * Confidential Information and shall use it only in accordance
- * with the terms of the license agreement you entered into.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software Foundation
+ *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
 package ch.o2it.weblounge.common.site;
