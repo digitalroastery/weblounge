d53040a4ae6644182bf5a8d8076e239642f371cb
Tobias Wunden <tobias@entwinemedia.com>
2009-11-27 23:39:31 +0000

    Added comparator support to Localizable

--- src/main/java/ch/o2it/weblounge/common/language/Localizable.java
+++ src/main/java/ch/o2it/weblounge/common/language/Localizable.java
@@ -55,7 +55,7 @@ public interface Localizable {
   Set<Language> languages();
 
   /**
-   * Makes <code>language</code> the current language for this object.
+   * Makes <code>language</code> the current language for this localizable.
    * <p>
    * Depending on whether the localizable supports the language, a fall back
    * might be chosen. This can be determined by comparing the returned language
@@ -71,5 +71,52 @@ public interface Localizable {
    *           if no fall back language can be determined
    */
   Language switchTo(Language language);
+
+  /**
+   * Compares this Localizable with the specified Localizable for order. Returns
+   * a negative integer, zero, or a positive integer as this object is less
+   * than, equal to, or greater than the specified object.
+   * 
+   * <p>
+   * The implementor must ensure <tt>sgn(x.compareTo(y)) ==
+   * -sgn(y.compareTo(x))</tt> for all <tt>x</tt> and <tt>y</tt>. (This implies
+   * that <tt>x.compareTo(y)</tt> must throw an exception iff
+   * <tt>y.compareTo(x)</tt> throws an exception.)
+   * 
+   * <p>
+   * The implementor must also ensure that the relation is transitive:
+   * <tt>(x.compareTo(y)&gt;0 &amp;&amp; y.compareTo(z)&gt;0)</tt> implies
+   * <tt>x.compareTo(z)&gt;0</tt>.
+   * 
+   * <p>
+   * Finally, the implementor must ensure that <tt>x.compareTo(y)==0</tt>
+   * implies that <tt>sgn(x.compareTo(z)) == sgn(y.compareTo(z))</tt>, for all
+   * <tt>z</tt>.
+   * 
+   * <p>
+   * It is strongly recommended, but <i>not</i> strictly required that
+   * <tt>(x.compareTo(y)==0) == (x.equals(y))</tt>. Generally speaking, any
+   * class that implements the <tt>Comparable</tt> interface and violates this
+   * condition should clearly indicate this fact. The recommended language is
+   * "Note: this class has a natural ordering that is inconsistent with equals."
+   * 
+   * <p>
+   * In the foregoing description, the notation <tt>sgn(</tt><i>expression</i>
+   * <tt>)</tt> designates the mathematical <i>signum</i> function, which is
+   * defined to return one of <tt>-1</tt>, <tt>0</tt>, or <tt>1</tt> according
+   * to whether the value of <i>expression</i> is negative, zero or positive.
+   * 
+   * @param o
+   *          the object to be compared.
+   * @param l
+   *          the language to be used for the comparison
+   * @return a negative integer, zero, or a positive integer as this object is
+   *         less than, equal to, or greater than the specified object.
+   * 
+   * @throws ClassCastException
+   *           if the specified object's type prevents it from being compared to
+   *           this object.
+   */
+  int compareTo(Localizable o, Language l);
 
 }
