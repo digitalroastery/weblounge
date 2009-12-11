9a4c46f357a87a208d46eeca97024f4ec5beccea
Tobias Wunden <tobias@entwinemedia.com>
2009-12-11 16:08:31 +0000

    Moved templates to util package.

--- src/main/java/ch/o2it/weblounge/common/impl/util/Templates.java
+++ src/main/java/ch/o2it/weblounge/common/impl/util/Templates.java
@@ -18,7 +18,7 @@
  *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-package ch.o2it.weblounge.common.impl.page;
+package ch.o2it.weblounge.common.impl.util;
 
 import ch.o2it.weblounge.common.site.Site;
 
