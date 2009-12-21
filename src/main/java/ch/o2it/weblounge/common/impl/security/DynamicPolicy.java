f8de32dd806777cd0755d6584139dd39a05e5543
Tobias Wunden <tobias@entwinemedia.com>
2009-12-21 17:51:24 +0000

    First take at JAAS authorization

---
+++ src/main/java/ch/o2it/weblounge/common/impl/security/DynamicPolicy.java
@@ -0,0 +1,81 @@
+/*
+ *  Weblounge: Web Content Management System
+ *  Copyright (c) 2009 The Weblounge Team
+ *  http://weblounge.o2it.ch
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software Foundation
+ *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+package ch.o2it.weblounge.common.impl.security;
+
+import java.security.CodeSource;
+import java.security.PermissionCollection;
+import java.security.Policy;
+import java.security.Principal;
+import java.security.ProtectionDomain;
+
+/**
+ * TODO: Comment XMLPolicy
+ */
+public class DynamicPolicy extends Policy {
+
+  /** The original JAAS policy */
+  private Policy deferredPolicy = null;
+
+  /**
+   * Creates a new custom policy, backed by <code>deferredPolicy</code> in order
+   * to gather permissions defined by the system.
+   * 
+   * @param deferredPolicy
+   *          the original policy
+   */
+  public DynamicPolicy(Policy deferredPolicy) {
+    this.deferredPolicy = deferredPolicy;
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see java.security.Policy#getPermissions(java.security.CodeSource)
+   */
+  @Override
+  public PermissionCollection getPermissions(CodeSource codesource) {
+    return deferredPolicy.getPermissions(codesource);
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see java.security.Policy#getPermissions(java.security.ProtectionDomain)
+   */
+  @Override
+  public PermissionCollection getPermissions(ProtectionDomain domain) {
+    PermissionCollection permissions = deferredPolicy.getPermissions(domain);
+    Principal[] principals = domain.getPrincipals();
+    for (Principal principal : principals) {
+      // TODO: Translate custom/well-known principals into permissions
+    }
+    return permissions;
+  }
+  
+  /**
+   * {@inheritDoc}
+   * @see java.security.Policy#refresh()
+   */
+  @Override
+  public void refresh() {
+    deferredPolicy.refresh();
+  }
+
+}
---
+++ src/main/java/ch/o2it/weblounge/common/impl/security/ResourcePermission.java
@@ -0,0 +1,106 @@
+/*
+ *  Weblounge: Web Content Management System
+ *  Copyright (c) 2009 The Weblounge Team
+ *  http://weblounge.o2it.ch
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software Foundation
+ *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+package ch.o2it.weblounge.common.impl.security;
+
+import ch.o2it.weblounge.common.security.Securable;
+
+import java.security.Permission;
+
+/**
+ * TODO: Comment ResourcePermission
+ */
+public class ResourcePermission extends Permission {
+
+  /** Serial version UID */
+  private static final long serialVersionUID = 1073544616602445043L;
+  
+  /** The action that is to be executed on the resource */
+  private String action = null;
+  
+  /** The securable */
+  private Securable resource = null;
+
+  /**
+   * Creates a new resource permission with the given name.
+   * 
+   * @param name
+   *          the permission name
+   * @param action
+   *          the action that is to be taken
+   * @param resource
+   *          the resource that is to be accessed
+   */
+  public ResourcePermission(String name, String action, Securable resource) {
+    super(name);
+    this.action = action;
+    if (resource == null)
+      throw new IllegalArgumentException("Resource cannot be null");
+    this.resource = resource;
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see java.security.Permission#equals(java.lang.Object)
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof ResourcePermission) {
+      ResourcePermission p = (ResourcePermission)obj;
+      return getName().equals(p.getName()) &&
+            (resource.equals(p.resource)) &&
+            ((action == null && p.action == null) || action.equals(p.action));
+    }
+    return false;
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see java.security.Permission#getActions()
+   */
+  @Override
+  public String getActions() {
+    return action;
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see java.security.Permission#hashCode()
+   */
+  @Override
+  public int hashCode() {
+    return getName().hashCode();
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see java.security.Permission#implies(java.security.Permission)
+   */
+  @Override
+  public boolean implies(Permission permission) {
+    // TODO: Finish implementation
+    return false;
+  }
+
+}
