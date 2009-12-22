47c0139914a74f2c7b1838c683acba98e5060863
Tobias Wunden <tobias@entwinemedia.com>
2009-12-22 15:26:36 +0000

    Integrated cache support into weblounge response.

--- src/main/java/ch/o2it/weblounge/cache/CacheService.java
+++ src/main/java/ch/o2it/weblounge/cache/CacheService.java
@@ -22,148 +22,14 @@ package ch.o2it.weblounge.cache;
 
 import ch.o2it.weblounge.common.content.Tag;
 import ch.o2it.weblounge.common.request.CacheHandle;
-import ch.o2it.weblounge.common.request.WebloungeRequest;
-import ch.o2it.weblounge.common.request.WebloungeResponse;
+import ch.o2it.weblounge.common.request.ResponseCache;
 
 import java.util.Set;
-
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * TODO: Comment CacheService
  */
-public interface CacheService {
-
-  /**
-   * Starts a cacheable response. By calling this method, a new response wrapper
-   * is generated which will write the response output to the cache as well as
-   * to the client.
-   * <p>
-   * If the method returns <code>true</code>, then the response part was found
-   * in the cache and has been directly written to the response from the cache.
-   * If it returns <code>false</code>, the data was not found but will be put
-   * into the cache when {@link #endResponse(WebloungeResponse)} is called.
-   * 
-   * @param uniqueTags
-   *          the tags identifying this response
-   * @param request
-   *          the request
-   * @param response
-   *          the response
-   * @param validTime
-   *          the valid time in milliseconds
-   * @param recheckTime
-   *          the recheck time in milliseconds
-   * @return the <code>CacheHandle</code> of the response or <code>null</code>
-   *         if the response was found in the cache
-   */
-  CacheHandle startResponse(Iterable<Tag> uniqueTags, WebloungeRequest request,
-      WebloungeResponse response, long validTime, long recheckTime);
-
-  /**
-   * Starts a cacheable response. By calling this method, a new response wrapper
-   * is generated which will write the response output to the cache as well as
-   * to the client.
-   * <p>
-   * If the method returns <code>true</code>, then the response part was found
-   * in the cache and has been directly written to the response from the cache.
-   * If it returns <code>false</code>, the data was not found but will be put
-   * into the cache when {@link #endResponse(WebloungeResponse)} is called.
-   * 
-   * @param request
-   *          the request
-   * @param response
-   *          the response
-   * @return boolean <code>true</code> if the response was found in the cache
-   */
-  boolean startResponse(CacheHandle handle, WebloungeRequest request,
-      WebloungeResponse response);
-
-  /**
-   * Tell the cache service that writing the response to the client is now
-   * finished and that the cache buffer containing the response may be written to
-   * the cache.
-   * 
-   * @param response
-   *          the servlet response
-   */
-  boolean endResponse(WebloungeResponse response);
-
-  /**
-   * Tell the cache service that writing the response to the client failed for
-   * some reason. In this case, the cache will discard the cached data for this
-   * response and directly write the data out to the client.
-   * <p>
-   * Once this method has been called, all subsequent calls to
-   * <code>endResponse</code>, <code>startResponsePart</code> and
-   * <code>endtResponsePart</code> will have no effect.
-   * 
-   * @param response
-   *          the servlet response
-   */
-  void invalidateResponse(WebloungeResponse response);
-
-  /**
-   * Starts caching a sub portion of the current response, identified by a set
-   * of cache tags.
-   * <p>
-   * Dividing the cached response into parts has the advantage, that, if for
-   * example on part of a page becomes invalid, the other parts remain in the
-   * cache and only the invalidated part and the page in whole have to be
-   * rebuilt.
-   * <p>
-   * If the method returns <code>true</code>, then the response part was found
-   * in the cache and has been directly written to the response from the cache.
-   * If it returns <code>false</code>, the data was not found but will be put
-   * into the cache.
-   * 
-   * @param uniqueTags
-   *          the tag set identifying the response part
-   * @param response
-   *          the servlet response
-   * @param validTime
-   *          the valid time in milliseconds
-   * @param recheckTime
-   *          the recheck time in milliseconds
-   * @return the <code>CacheHandle</code> of the responose part or
-   *         <code>null</code> if the response part was found in the cache
-   */
-  CacheHandle startResponsePart(Iterable<Tag> uniqueTags,
-      HttpServletResponse response, long validTime, long recheckTime);
-
-  /**
-   * Starts caching a sub portion of the current response, identified by
-   * <code>handle</code>. Dividing the cached response into parts has the
-   * advantage, that, if for example on part of a page becomes invalid, the
-   * other parts remain in the cache and only the invalidated part and the page
-   * in whole have to be rebuilt.
-   * <p>
-   * If the method returns <code>true</code>, then the response part was found
-   * in the cache and has been directly written to the response from the cache.
-   * If it returns <code>false</code>, the data was not found but will be put
-   * into the cache.
-   * 
-   * @param handle
-   *          the response part identifier
-   * @param response
-   *          the servlet response
-   * @return boolean <code>true</code> if the response part was found in the
-   *         cache
-   */
-  boolean startResponsePart(CacheHandle handle, HttpServletResponse response);
-
-  /**
-   * Tells the cache manager that the data identified by <code>handle</code> is
-   * complete and may be written to the cache.
-   * 
-   * @param handle
-   *          the response part identifier. <br>
-   *          NOTE: This MUST be the same instance that was used to start the
-   *          corresponding response part!
-   * @param response
-   *          the servlet response
-   */
-  void endResponsePart(CacheHandle handle, HttpServletResponse response);
+public interface CacheService extends ResponseCache {
 
   /**
    * Tells the cache manager to throw away the data identified by
@@ -173,15 +39,6 @@ public interface CacheService {
    *          the cache data identifier
    */
   Set<CacheHandle> invalidate(Iterable<Tag> tags);
-
-  /**
-   * Tells the cache manager to throw away the data identified by
-   * <code>handle</code>..
-   * 
-   * @param handle
-   *          the cache data identifier
-   */
-  Set<CacheHandle> invalidateEntry(CacheHandle handle);
 
   /**
    * Sets the maximum cache size to the given number of bytes.
