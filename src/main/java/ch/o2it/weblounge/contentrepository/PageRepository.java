f83d07104e67a8e22ceb7dd5851ed4d8f5fd490a
Tobias Wunden <tobias@entwinemedia.com>
2010-01-30 03:50:55 +0000

    First take at the content repository.

--- src/main/java/ch/o2it/weblounge/contentrepository/PageRepository.java
+++ src/main/java/ch/o2it/weblounge/contentrepository/PageRepository.java
@@ -20,10 +20,71 @@
 
 package ch.o2it.weblounge.contentrepository;
 
+import ch.o2it.weblounge.common.page.Page;
+import ch.o2it.weblounge.common.page.PageURI;
+import ch.o2it.weblounge.common.page.SearchQuery;
+import ch.o2it.weblounge.common.page.SearchResult;
+
 /**
  * A <code>PageRepository</code> is a facility that serves pages to weblounge
- * sites. 
+ * sites.
  */
 public interface PageRepository {
+
+  /**
+   * Returns the page identified by <code>uri</code> or <code>null</code> if no
+   * page was found at the specified location or revision.
+   * 
+   * @param uri
+   *          the page uri
+   * @return the page
+   */
+  Page get(PageURI uri);
+
+  /**
+   * Puts the page to the specified location. Depending on whether the page
+   * identified by <code>uri</code> already exists, the method either creates a
+   * new page or updates the existing one.
+   * <p>
+   * The returned page contains the same data than the one passed in as the
+   * <code>page</code> argument but with an updated uri.
+   * 
+   * @param uri
+   *          the page uri
+   * @param page
+   *          the page
+   * @return the page with the given uri
+   */
+  Page put(PageURI uri, Page page);
+
+  /**
+   * Removes the indicated page and revision from the content repository. If
+   * <code>allRevisisions</code> is set to <code>true</code>, all revisions will
+   * be removed.
+   * 
+   * @param uri
+   *          the page uri
+   * @param allRevisions
+   *          <code>true</code> to remove all revisions
+   */
+  void delete(PageURI uri, boolean allRevisions);
+
+  /**
+   * Returns a page uri for every available revision of the page.
+   * 
+   * @param uri
+   *          the page uri
+   * @return the revisions
+   */
+  PageURI[] getVersions(PageURI uri);
+
+  /**
+   * Returns the search results for <code>query</code>.
+   * 
+   * @param query
+   *          the query
+   * @return the search result
+   */
+  SearchResult[] findPages(SearchQuery query);
 
 }
