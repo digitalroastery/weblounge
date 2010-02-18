d8d594cebb472703dc3a1ae87aee8b0dd7659731
Tobias Wunden <tobias@entwinemedia.com>
2010-02-18 05:55:12 +0000

    Added preliminaries for scheduler tests.

---
+++ src/test/java/ch/o2it/weblounge/common/scheduler/TestJob.java
@@ -0,0 +1,104 @@
+/*
+ *  Weblounge: Web Content Management System
+ *  Copyright (c) 2010 The Weblounge Team
+ *  http://weblounge.o2it.ch
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software Foundation
+ *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+package ch.o2it.weblounge.common.scheduler;
+
+import java.io.Serializable;
+import java.util.Dictionary;
+
+/**
+ * Test implementation for a job, used by the scheduler test cases.
+ * <p>
+ * The behavior of this implementation is as follows: On every execution, it
+ * will update the value of {@link #CTX_EXECUTIONS} in the context, while making
+ * sure it fit the expected value. In case of a mismatch, it will throw a
+ * {@link JobException}.
+ * <p>
+ * A <code>JobException</code> will also be thrown on the third execution of the
+ * job in order to allow for proper testing of exception handling. In addition,
+ * on the tenth execution of the job, a {@link ArithmeticException} will be
+ * thrown as an example of a <code>RuntimeException</code> as opposed to the
+ * checked <code>JobException</code>.
+ */
+public class TestJob implements Job {
+
+  /** String constant identifying the number of executions */
+  public static final String CTX_EXECUTIONS = "executions";
+
+  /** Execution where a <code>JobException</code> will be thrown */
+  public static final int JOB_EXCEPTION_COUNT = 3;
+
+  /** Execution where a <code>ArithmeticException</code> will be thrown */
+  public static final int ARITHMETIC_EXCEPTION_COUNT = 10;
+
+  /** Keep track of how often we have been called */
+  private int executions = 0;
+
+  /** The last context */
+  private Dictionary<String, Serializable> lastContext = null;
+
+  /**
+   * Returns the number of times that this job has been called.
+   * 
+   * @return the number of executions
+   */
+  public int getExecutions() {
+    return executions;
+  }
+
+  /**
+   * Returns the last context that was passed in at job execution.
+   * 
+   * @return the last context
+   */
+  public Dictionary<String, Serializable> getLastContext() {
+    return lastContext;
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see ch.o2it.weblounge.common.scheduler.Job#execute(java.lang.String,
+   *      java.util.Dictionary)
+   */
+  public void execute(String name, Dictionary<String, Serializable> ctx)
+      throws JobException {
+    if (executions > 0) {
+      Integer storedInCtx = (Integer) ctx.get(CTX_EXECUTIONS);
+      if (storedInCtx == null)
+        throw new JobException(this, "Context does not contain previously stored value");
+      if (storedInCtx.intValue() != executions)
+        throw new JobException(this, "Context contain wrong value (" + storedInCtx + ", while expecting " + executions + ")");
+    }
+
+    // Increase execution count
+    executions ++;
+
+    // Update the context
+    lastContext = ctx;
+    ctx.put(CTX_EXECUTIONS, Integer.valueOf(executions));
+    
+    if (executions == JOB_EXCEPTION_COUNT)
+      throw new JobException(this, "The third execution always triggers this exception");
+    if (executions == ARITHMETIC_EXCEPTION_COUNT)
+      throw new ArithmeticException("The tenth execution always triggers this exception");
+  }
+
+}
---
+++ src/test/java/ch/o2it/weblounge/common/scheduler/TestJobTest.java
@@ -0,0 +1,122 @@
+/*
+ *  Weblounge: Web Content Management System
+ *  Copyright (c) 2010 The Weblounge Team
+ *  http://weblounge.o2it.ch
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software Foundation
+ *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+package ch.o2it.weblounge.common.scheduler;
+
+import static org.junit.Assert.assertNotNull;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.Serializable;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+/**
+ * Test case for class {@link TestJob}. This test ensures that the test job is
+ * actually exhibiting expected test behavior and is running well, at least
+ * outside of the scheduler context.
+ */
+public class TestJobTest {
+
+  /** The test job */
+  protected TestJob job = null;
+
+  /** Name for the test job */
+  protected String jobName = "testjob";
+
+  /** Job context */
+  protected Dictionary<String, Serializable> jobContext = null;
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @Before
+  public void setUp() throws Exception {
+    job = new TestJob();
+    jobContext = new Hashtable<String, Serializable>();
+  }
+
+  /**
+   * Test method for
+   * {@link ch.o2it.weblounge.common.scheduler.TestJob#getExecutions()}.
+   */
+  @Test
+  public void testGetExecutions() {
+    for (int i = 1; i < 3; i++) {
+      try {
+        job.execute(jobName, jobContext);
+        assertEquals(i, job.getExecutions());
+      } catch (JobException e) {
+        fail(e.getMessage());
+      }
+    }
+  }
+
+  /**
+   * Test method for
+   * {@link ch.o2it.weblounge.common.scheduler.TestJob#getLastContext()}.
+   */
+  @Test
+  public void testGetLastContext() {
+    for (int i = 1; i < 3; i++) {
+      try {
+        job.execute(jobName, jobContext);
+        assertNotNull(job.getLastContext());
+        assertNotNull(job.getLastContext().get(TestJob.CTX_EXECUTIONS));
+        assertEquals(i, job.getLastContext().get(TestJob.CTX_EXECUTIONS));
+      } catch (JobException e) {
+        fail(e.getMessage());
+      }
+    }
+  }
+
+  /**
+   * Test method for
+   * {@link ch.o2it.weblounge.common.scheduler.TestJob#execute(java.lang.String, java.util.Dictionary)}
+   * .
+   */
+  @Test
+  public void testExecute() {
+    for (int i = 1; i < 15; i++) {
+      try {
+        job.execute(jobName, jobContext);
+        switch (i) {
+          case TestJob.JOB_EXCEPTION_COUNT:
+          case TestJob.ARITHMETIC_EXCEPTION_COUNT:
+            fail("The job should have failed during execution " + i);
+            break;
+          default:
+            break;
+        }
+      } catch (JobException e) {
+        if (i != TestJob.JOB_EXCEPTION_COUNT)
+          fail("Job throw JobException during run " + i + " instead of " + TestJob.JOB_EXCEPTION_COUNT);
+      } catch (Throwable t) {
+        if (i != TestJob.ARITHMETIC_EXCEPTION_COUNT)
+          fail("Job throw JobException during run " + i + " instead of " + TestJob.ARITHMETIC_EXCEPTION_COUNT);
+      }
+    }
+  }
+
+}
