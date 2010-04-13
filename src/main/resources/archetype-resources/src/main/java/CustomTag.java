eb9f6f458196bb9d42c4a2745ca343664516d2da
Tobias Wunden <tobias@entwinemedia.com>
2010-04-13 22:52:14 +0000

    Initial import of the site plugin

---
+++ src/main/resources/archetype-resources/src/main/java/CustomTag.java
@@ -0,0 +1,59 @@
+/*
+ *  Weblounge: Web Content Management System
+ *  Copyright (c) 2010 The Weblounge Team
+ *  http://weblounge.o2it.ch
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software Foundation
+ *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+package ${groupId};
+
+import java.io.IOException;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+/**
+ * Sample tag implementation.
+ */
+public class CustomTag extends TagSupport {
+
+  /** Serial version uid */
+  private static final long serialVersionUID = 633023845095031930L;
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see javax.servlet.jsp.tagext.TagSupport#doStartTag()
+   */
+  public int doStartTag() throws JspException {
+    try {
+      pageContext.getOut().print("<h1>Hello World</h1>");
+      pageContext.getOut().flush();
+    } catch (IOException ioe) {
+      throw new JspException("IOException while writing to page" + ioe.getMessage());
+    }
+    return SKIP_BODY;
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @see javax.servlet.jsp.tagext.TagSupport#doEndTag()
+   */
+  public int doEndTag() throws JspException {
+    return EVAL_PAGE;
+  }
+
+}
---
+++ src/main/resources/archetype-resources/src/main/resources/site/templates/template.jsp
@@ -0,0 +1,9 @@
+<%@ taglib uri="/WEB-INF/taglibrary.tld" prefix="custom" %>
+<html>
+	<head>
+		<title>Welcome to ${groupId}</title>
+	</head>
+	<body>
+		<custom:greeting/>
+	</body>
+</html>
