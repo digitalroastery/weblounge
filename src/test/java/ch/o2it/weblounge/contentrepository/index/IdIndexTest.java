6cdaaf0d81593dfddffb125bcfb7f8452e1072bf
Tobias Wunden <tobias@entwinemedia.com>
2010-05-04 09:16:22 +0000

    Organized imports.

--- src/test/java/ch/o2it/weblounge/contentrepository/index/IdIndexTest.java
+++ src/test/java/ch/o2it/weblounge/contentrepository/index/IdIndexTest.java
@@ -20,10 +20,8 @@
 
 package ch.o2it.weblounge.contentrepository.index;
 
-import static org.junit.Assert.assertTrue;
-
 import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import ch.o2it.weblounge.contentrepository.impl.index.IdIndex;
