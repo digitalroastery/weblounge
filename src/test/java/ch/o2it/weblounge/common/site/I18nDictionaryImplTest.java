178266630bbed90aad4eb657b9f5715e82e96f17
Tobias Wunden <tobias@entwinemedia.com>
2010-05-11 21:10:50 +0000

    Started work on i18n integration

---
+++ src/test/java/ch/o2it/weblounge/common/site/I18nDictionaryImplTest.java
@@ -0,0 +1,122 @@
+/*
+ *  Weblounge: Web Content Management System
+ *  Copyright (c) 2010 The Weblounge Team
+ *  http://weblounge.o2it.ch
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software Foundation
+ *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+package ch.o2it.weblounge.common.site;
+
+import static org.junit.Assert.fail;
+
+import ch.o2it.weblounge.common.impl.site.I18nDictionaryImpl;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test case for {@link I18nDictionaryImpl}.
+ */
+public class I18nDictionaryImplTest {
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  /**
+   * Test method for {@link ch.o2it.weblounge.common.impl.site.I18nDictionaryImpl#setDefaultLanguage(ch.o2it.weblounge.common.language.Language)}.
+   */
+  @Test
+  public void testSetDefaultLanguage() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link ch.o2it.weblounge.common.impl.site.I18nDictionaryImpl#add(java.lang.String, java.lang.String, ch.o2it.weblounge.common.language.Language)}.
+   */
+  @Test
+  public void testAdd() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link ch.o2it.weblounge.common.impl.site.I18nDictionaryImpl#get(java.lang.String, ch.o2it.weblounge.common.language.Language)}.
+   */
+  @Test
+  public void testGetStringLanguage() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link ch.o2it.weblounge.common.impl.site.I18nDictionaryImpl#get(java.lang.String, ch.o2it.weblounge.common.language.Language, boolean)}.
+   */
+  @Test
+  public void testGetStringLanguageBoolean() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link ch.o2it.weblounge.common.impl.site.I18nDictionaryImpl#getAsHTML(java.lang.String, ch.o2it.weblounge.common.language.Language)}.
+   */
+  @Test
+  public void testGetAsHTMLStringLanguage() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link ch.o2it.weblounge.common.impl.site.I18nDictionaryImpl#getAsHTML(java.lang.String, ch.o2it.weblounge.common.language.Language, boolean)}.
+   */
+  @Test
+  public void testGetAsHTMLStringLanguageBoolean() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link ch.o2it.weblounge.common.impl.site.I18nDictionaryImpl#remove(java.lang.String, ch.o2it.weblounge.common.language.Language)}.
+   */
+  @Test
+  public void testRemoveStringLanguage() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link ch.o2it.weblounge.common.impl.site.I18nDictionaryImpl#remove(java.lang.String)}.
+   */
+  @Test
+  public void testRemoveString() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link ch.o2it.weblounge.common.impl.site.I18nDictionaryImpl#addDictionary(java.io.File)}.
+   */
+  @Test
+  public void testAddDictionaryFile() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link ch.o2it.weblounge.common.impl.site.I18nDictionaryImpl#addDictionary(java.io.File, boolean)}.
+   */
+  @Test
+  public void testAddDictionaryFileBoolean() {
+    fail("Not yet implemented"); // TODO
+  }
+
+}
