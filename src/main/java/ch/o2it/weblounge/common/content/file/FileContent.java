234f6530d05e6265f053f34d3dfda58cc4902e53
Tobias Wunden <tobias@entwinemedia.com>
2010-08-10 07:17:34 +0000

    Created object representations for file and image content [#228]

---
+++ src/main/java/ch/o2it/weblounge/common/content/file/FileContent.java
@@ -0,0 +1,99 @@
+/*
+ *  Weblounge: Web Content Management System
+ *  Copyright (c) 2010 The Weblounge Team
+ *  http://weblounge.o2it.ch
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software Foundation
+ *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+package ch.o2it.weblounge.common.content.file;
+
+import ch.o2it.weblounge.common.language.Language;
+
+/**
+ * File content represents an actual download for a {@link FileResource}. It
+ * contains information about the file's mime type, size and extension. In
+ * addition, it is designed to be extended by subclasses like images.
+ */
+public interface FileContent {
+
+  /**
+   * Sets the content language.
+   * 
+   * @param language
+   *          the language
+   */
+  void setLanguage(Language language);
+
+  /**
+   * Returns the file language.
+   * 
+   * @return the language
+   */
+  Language getLanguage();
+
+  /**
+   * Sets the content mime type.
+   * 
+   * @param mimetype
+   *          the mime type
+   */
+  void setMimetype(String mimetype);
+
+  /**
+   * Returns the content type.
+   * 
+   * @return the mime type
+   */
+  String getMimetype();
+
+  /**
+   * Sets the filename.
+   * 
+   * @param filename
+   *          the filename
+   */
+  void setFilename(String filename);
+
+  /**
+   * Returns the original filename.
+   * 
+   * @return the filename
+   */
+  String getFilename();
+
+  /**
+   * Sets the file size in bytes.
+   * 
+   * @param size
+   *          the size
+   */
+  void setSize(long size);
+
+  /**
+   * Returns the file size in bytes.
+   * 
+   * @return the file size
+   */
+  long getSize();
+
+  /**
+   * Returns an xml representation of this file content.
+   * 
+   * @return the xml representation
+   */
+  String toXml();
+
+}
---
+++ src/main/java/ch/o2it/weblounge/common/content/image/ImageContent.java
@@ -0,0 +1,62 @@
+/*
+ *  Weblounge: Web Content Management System
+ *  Copyright (c) 2010 The Weblounge Team
+ *  http://weblounge.o2it.ch
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software Foundation
+ *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+package ch.o2it.weblounge.common.content.image;
+
+import ch.o2it.weblounge.common.content.file.FileContent;
+
+/**
+ * Describes the contents of an image resource, including the general attributes
+ * such as file size and mime type. In addition, it contains technical
+ * information like resolution, color model etc.
+ */
+public interface ImageContent extends FileContent {
+
+  /**
+   * Sets the image width in pixels.
+   * 
+   * @param width
+   *          the image width
+   */
+  void setWidth(int width);
+
+  /**
+   * Returns the image width in pixels.
+   * 
+   * @return the width
+   */
+  int getWidth();
+
+  /**
+   * Sets the image height in pixels.
+   * 
+   * @param height
+   *          the image height
+   */
+  void setHeight(int height);
+
+  /**
+   * Returns the image height in pixels.
+   * 
+   * @return the height
+   */
+  int getHeight();
+
+}
