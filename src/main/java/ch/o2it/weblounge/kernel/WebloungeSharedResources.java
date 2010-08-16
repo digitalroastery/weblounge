2bccfd81e4dee42472097ce125dc8e52c6766351
Tobias Wunden <tobias@entwinemedia.com>
2010-08-16 10:49:07 +0000

    Made the shared resources configurable. Adjusted the rest services to /system/weblounge/xyz

--- src/main/java/ch/o2it/weblounge/kernel/WebloungeSharedResources.java
+++ src/main/java/ch/o2it/weblounge/kernel/WebloungeSharedResources.java
@@ -20,30 +20,51 @@
 
 package ch.o2it.weblounge.kernel;
 
+import org.apache.commons.lang.StringUtils;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.http.HttpContext;
 import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Dictionary;
 
 /**
  * This component registers this bundle's <code>/html</code> resource directory
  * with the http service under <code>/system/shared/</code> so that images,
  * cascading stylesheets and javascripts are available locally.
  */
-public class WebloungeSharedResources {
+public class WebloungeSharedResources implements ManagedService {
 
   /** Logger */
   private static final Logger logger = LoggerFactory.getLogger(WebloungeSharedResources.class);
 
+  /** Service pid, used to look up the service configuration */
+  public static final String SERVICE_PID = "ch.o2it.weblounge.resources";
+
   /** Bundle directory containing the shared resources */
-  public static final String SHARED_RESOURCES_BUNDLE_DIR = "/html";
+  public static final String RESOURCES_BUNDLE_DIR = "/html";
 
   /** Mountpoint to use when registering the shared resources */
-  public static final String SHARED_RESOURCES_MOUNTPOINT = "/system/resources";
+  public static final String DEFAULT_RESOURCES_MOUNTPOINT = "/system/weblounge/shared";
+
+  /** Option name for the shared resources mountpoint */
+  public static final String OPT_RESOURCES_MOUNTPOINT = "resources.path";
+
+  /** Actual mountpoint for the shared resources */
+  private String resourcesMountpoint = null;
   
   /** The http service */
   private HttpService httpService = null;
+  
+  /** The http context */
+  private HttpContext httpContext = null;
 
   /**
    * Callback for OSGi's declarative services component dactivation.
@@ -54,9 +75,26 @@ public class WebloungeSharedResources {
    *           if component inactivation fails
    */
   void activate(ComponentContext context) throws Exception {
-    logger.info("Starting to serve shared weblounge resources at {}", SHARED_RESOURCES_MOUNTPOINT);
-    HttpContext httpContext = httpService.createDefaultHttpContext();
-    httpService.registerResources(SHARED_RESOURCES_MOUNTPOINT, SHARED_RESOURCES_BUNDLE_DIR, httpContext);
+    BundleContext bundleContext = context.getBundleContext();
+
+    // Try to get hold of the service configuration
+    ServiceReference configAdminRef = bundleContext.getServiceReference(ConfigurationAdmin.class.getName());
+    if (configAdminRef != null) {
+      ConfigurationAdmin configAdmin = (ConfigurationAdmin) bundleContext.getService(configAdminRef);
+      Dictionary<?, ?> config = configAdmin.getConfiguration(SERVICE_PID).getProperties();      
+      if (config != null) {
+        updated(config);
+      } else {
+        logger.debug("No customized configuration for content repository found");
+        resourcesMountpoint = DEFAULT_RESOURCES_MOUNTPOINT;
+      }
+    } else {
+      logger.debug("No configuration admin service found while looking for content repository configuration");
+    }
+
+    logger.info("Starting to serve shared weblounge resources at {}", resourcesMountpoint);
+    httpContext = httpService.createDefaultHttpContext();
+    httpService.registerResources(resourcesMountpoint, RESOURCES_BUNDLE_DIR, httpContext);
   }
 
   /**
@@ -69,7 +107,38 @@ public class WebloungeSharedResources {
    */
   void deactivate(ComponentContext context) throws Exception {
     logger.info("Stopping serving of shared weblounge resources", this);
-    httpService.unregister(SHARED_RESOURCES_MOUNTPOINT);
+    httpService.unregister(resourcesMountpoint);
+    httpContext = null;
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @see org.osgi.service.cm.ManagedService#updated(java.util.Dictionary)
+   */
+  @SuppressWarnings("rawtypes")
+  public void updated(Dictionary properties) throws ConfigurationException {
+    if (properties == null)
+      return;
+    
+    // Mountpoint
+    String currentMountpoint = resourcesMountpoint;
+    String mountpoint = (String)properties.get(OPT_RESOURCES_MOUNTPOINT);
+    if (StringUtils.trimToNull(mountpoint) != null) {
+      resourcesMountpoint = mountpoint;
+      logger.debug("Configured value for the shared resource mountpoint is '{}'", mountpoint);
+    }
+    
+    // Update the registration
+    if (currentMountpoint != null) {
+      try {
+        httpService.unregister(currentMountpoint);
+        logger.info("No serving shared weblounge resources at {}", resourcesMountpoint);
+        httpService.registerResources(resourcesMountpoint, RESOURCES_BUNDLE_DIR, httpContext);
+      } catch (NamespaceException e) {
+        logger.error("Error registering shared resources at " + resourcesMountpoint, e);
+      }
+    }
   }
 
   /**
