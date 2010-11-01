df4cbbc4a45fde240c94a4f673fa00f6b5561743
Tobias Wunden <tobias@entwinemedia.com>
2010-11-01 22:32:18 +0000

    Made sure that request properties are being passed on when wrapping the request.

--- src/main/java/ch/o2it/weblounge/dispatcher/impl/SiteServlet.java
+++ src/main/java/ch/o2it/weblounge/dispatcher/impl/SiteServlet.java
@@ -334,5 +334,15 @@ public class SiteServlet extends HttpServlet {
       logger.error("Wow, certainly didn't expect this to happen!", t);
     }
   }
+  
+  /**
+   * {@inheritDoc}
+   *
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public String toString() {
+    return "site " + site;
+  }
 
 }
