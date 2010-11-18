7dd44b9b547fb60ac6fdc1e8464c08dd52dc2ccb
Tobias Wunden <tobias@entwinemedia.com>
2010-11-18 17:07:49 +0000

    Set java.io.tmpdir to <felix.home>/work [#276]

--- docs/felix/bin/start.bat
+++ docs/felix/bin/start.bat
@@ -10,7 +10,8 @@ REM # Make sure the following two path entries do *not* contain spaces
 REM # Felix home
 SET FELIX_HOME=C:\Libraries\felix-framework-2.0.0
 SET FELIX_LOGDIR=%FELIX_HOME%\logs
-SET FELIX_CACHEDIR=%FELIX_HOME%\felix
+SET FELIX_CACHEDIR=%FELIX_HOME%\cache
+SET FELIX_TEMPDIR=%FELIX_HOME%\work
 
 REM # Maven home
 SET M2_REPO=C:\Users\johndoe\.m2\repository
@@ -28,6 +29,7 @@ SET FELIX_FILEINSTALL_OPTS=-Dfelix.fileinstall.dir=%FELIX_HOME%\load
 SET PAX_CONFMAN_OPTS=-Dbundles.configuration.location=%FELIX_HOME%\conf -Dweblounge.logdir=%FELIX_LOGDIR%
 SET PAX_LOGGING_OPTS=-Dorg.ops4j.pax.logging.DefaultServiceLog.level=WARN
 SET GRAPHICS_OPTS=-Djava.awt.headless=true -Dawt.toolkit=sun.awt.HeadlessToolkit
+SET TEMPDIR_OPTS=-Djava.io.tmpdir=$FELIX_TEMPDIR
 
 REM # Create the debug config
 SET DEBUG_OPTS=-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=%DEBUG_PORT%,server=y,suspend=%DEBUG_SUSPEND%
@@ -35,5 +37,5 @@ SET DEBUG_OPTS=-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=%DEBUG_PO
 REM # Remove the weblounge bundles from felix cache
 
 REM # Finally start felix
-java %DEBUG_OPTS% %MAVEN_OPTS% %GRAPHICS_OPTS% %FELIX_FILEINSTALL_OPTS% %PAX_CONFMAN_OPTS% %PAX_LOGGING_OPTS% -jar %FELIX_HOME%\bin\felix.jar %FELIX_CACHEDIR%
+java %DEBUG_OPTS% %TEMPDIR_OPTS% %MAVEN_OPTS% %GRAPHICS_OPTS% %FELIX_FILEINSTALL_OPTS% %PAX_CONFMAN_OPTS% %PAX_LOGGING_OPTS% -jar %FELIX_HOME%\bin\felix.jar %FELIX_CACHEDIR%
 ENDLOCAL
--- docs/felix/bin/start.sh
+++ docs/felix/bin/start.sh
@@ -6,6 +6,7 @@
 FELIX="/Applications/Weblounge"
 FELIX_LOGDIR="$FELIX/logs"
 FELIX_CACHEDIR="$FELIX/cache"
+FELIX_TEMPDIR="$FELIX/work"
 
 # Maven home
 M2_REPO="/Users/johndoe/.m2/repository"
@@ -23,6 +24,7 @@ FELIX_FILEINSTALL_OPTS="-Dfelix.fileinstall.dir=$FELIX/load"
 PAX_CONFMAN_OPTS="-Dbundles.configuration.location=$FELIX/conf"
 PAX_LOGGING_OPTS="-Dorg.ops4j.pax.logging.DefaultServiceLog.level=WARN -Dweblounge.logdir=$FELIX_LOGDIR"
 GRAPHICS_OPTS="-Djava.awt.headless=true -Dawt.toolkit=sun.awt.HeadlessToolkit"
+TEMPDIR_OPTS="-Djava.io.tmpdir=$FELIX_TEMPDIR"
 
 # Create the debug config
 DEBUG_OPTS="-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=$DEBUG_PORT,server=y,suspend=$DEBUG_SUSPEND"
@@ -34,4 +36,4 @@ done
 
 # Finally start felix
 cd $FELIX
-java $DEBUG_OPTS $MAVEN_OPTS $GRAPHICS_OPTS $FELIX_FILEINSTALL_OPTS $PAX_CONFMAN_OPTS $PAX_LOGGING_OPTS -jar $FELIX/bin/felix.jar $FELIX_CACHEDIR
+java $DEBUG_OPTS $TEMPDIR_OPTS $MAVEN_OPTS $GRAPHICS_OPTS $FELIX_FILEINSTALL_OPTS $PAX_CONFMAN_OPTS $PAX_LOGGING_OPTS -jar $FELIX/bin/felix.jar $FELIX_CACHEDIR
