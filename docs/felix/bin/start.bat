bcf6638555904dc310b077ad4037496bba1fc574
Tobias Wunden <tobias@entwinemedia.com>
2011-01-28 17:44:14 +0000

    More fixes to the windows start script

--- docs/felix/bin/start.bat
+++ docs/felix/bin/start.bat
@@ -1,12 +1,6 @@
 @ECHO OFF
 SETLOCAL ENABLEEXTENSIONS
 
-REM ##
-REM # Configure these variables to match your environment
-REM # If you have system-wide variables for WEBLOUNGE_HOME and M2_REPO then you
-REM # should not have to make any changes to this file.
-REM ##
-
 REM # Main Weblounge path configuration. Make sure that the user executing Weblounge
 REM # has write access to WEBLOUNGE_WORKDIR.
 
@@ -22,7 +16,7 @@ SET DEBUG_PORT=8000
 SET DEBUG_SUSPEND=n
 
 REM # Detail configuration for weblounge directories. Usually, it is fine to simply
-REM # adjust the two paths above, namely $WEBLOUNGE_HOME and $WEBLOUNGE_WORKDIR.
+REM # adjust the two paths above, namely WEBLOUNGE_HOME% and WEBLOUNGE_WORKDIR.
 
 SET WEBLOUNGE_LOGDIR=%WEBLOUNGE_WORKDIR%\logs
 SET WEBLOUNGE_CACHEDIR=%WEBLOUNGE_WORKDIR%\cache
@@ -48,42 +42,6 @@ SET DEBUG_OPTS=-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=%DEBUG_PO
 REM # Create the java runtime options
 
 SET RUNTIME_OPTS=%WEBLOUNGE_OPTS% %TEMPDIR_OPTS% %GRAPHICS_OPTS% %WEBLOUNGE_FILEINSTALL_OPTS% %PAX_CONFMAN_OPTS% %PAX_LOGGING_OPTS%
-
-REM ##
-REM # Configure these variables to match your environment
-REM ##
-
-REM # Main Weblounge path configuration. Make sure that the user executing Weblounge
-REM # has write access to WEBLOUNGE_WORKDIR.
-
-SET WEBLOUNGE_HOME="/Applications/Weblounge"
-SET WEBLOUNGE_WORKDIR=$WEBLOUNGE_HOME
-
-REM # Detail configuration for weblounge directories. Usually, it is fine to simply
-REM # adjust the two paths above, namely $WEBLOUNGE_HOME and $WEBLOUNGE_WORKDIR.
-
-SET WEBLOUNGE_LOGDIR="$WEBLOUNGE_WORKDIR/logs"
-SET WEBLOUNGE_CACHEDIR="$WEBLOUNGE_WORKDIR/cache"
-SET WEBLOUNGE_TEMPDIR="$WEBLOUNGE_WORKDIR/work"
-SET WEBLOUNGE_SITESDIR="$WEBLOUNGE_WORKDIR/sites"
-SET WEBLOUNGE_SITESDATADIR="$WEBLOUNGE_WORKDIR/sites-data"
-
-REM ##
-REM # Only change the line below if you want to customize the server
-REM ##
-
-SET WEBLOUNGE_OPTS="-Dweblounge.sitesdir=$WEBLOUNGE_SITESDIR -Dweblounge.sitesdatadir=$WEBLOUNGE_SITESDATADIR"
-SET WEBLOUNGE_FILEINSTALL_OPTS="-Dfelix.fileinstall.dir=$WEBLOUNGE_HOME/load"
-SET PAX_CONFMAN_OPTS="-Dbundles.configuration.location=$WEBLOUNGE_HOME/conf"
-SET PAX_LOGGING_OPTS="-Dorg.ops4j.pax.logging.DefaultServiceLog.level=WARN -Dweblounge.logdir=$WEBLOUNGE_LOGDIR"
-SET GRAPHICS_OPTS="-Djava.awt.headless=true -Dawt.toolkit=sun.awt.HeadlessToolkit"
-SET TEMPDIR_OPTS="-Djava.io.tmpdir=$WEBLOUNGE_TEMPDIR"
-
-REM # Create the debug config
-SET DEBUG_OPTS="-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=$DEBUG_PORT,server=y,suspend=$DEBUG_SUSPEND"
-
-REM # Create the java runtime options
-SET RUNTIME_OPTS="$WEBLOUNGE_OPTS $TEMPDIR_OPTS $GRAPHICS_OPTS $WEBLOUNGE_FILEINSTALL_OPTS $PAX_CONFMAN_OPTS $PAX_LOGGING_OPTS"
 
 REM # Create the directories
 IF NOT EXIST %WEBLOUNGE_LOGDIR% MKDIR %WEBLOUNGE_LOGDIR%
