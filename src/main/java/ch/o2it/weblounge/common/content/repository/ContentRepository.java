b87d164e5dd323e03e1b4799a0700ac6cc4e325f
Tobias Wunden <tobias@entwinemedia.com>
2011-02-07 08:09:55 +0000

    [#184]

--- src/main/java/ch/o2it/weblounge/common/content/repository/ContentRepository.java
+++ src/main/java/ch/o2it/weblounge/common/content/repository/ContentRepository.java
@@ -78,24 +78,6 @@ public interface ContentRepository {
   void disconnect() throws ContentRepositoryException;
 
   /**
-   * Starts the repository. This is the perfect place to create or load indices,
-   * pre-populate caches etc.
-   * 
-   * @throws ContentRepositoryException
-   *           if starting the repository fails
-   */
-  void start() throws ContentRepositoryException;
-
-  /**
-   * Stops the repository. This method will usually be called prior to a call to
-   * {@#disconnect()}.
-   * 
-   * @throws ContentRepositoryException
-   *           if starting the repository fails
-   */
-  void stop() throws ContentRepositoryException;
-
-  /**
    * Returns the resource identified by <code>uri</code> or <code>null</code> if
    * no resource was found at the specified location or revision.
    * 
