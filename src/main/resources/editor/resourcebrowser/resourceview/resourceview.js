32d31e8757a59495a54e9e938222ae8b55b00a2b
Basil Brunner <basil@entwinemedia.com>
2011-05-20 16:59:52 +0000

    more work on the resource browser (editor)

--- src/main/resources/editor/resourcebrowser/resourceview/resourceview.js
+++ src/main/resources/editor/resourcebrowser/resourceview/resourceview.js
@@ -1,12 +1,31 @@
 steal.plugins().then(function($) {
 
-  $.Controller('Editor.Resourceview', 
-	{
-		
-		init: function(el) {
-			
-		}
-		
-	});
+  $.Controller('Editor.Resourceview', {
+
+    init: function(el) {
+
+    },
+
+    addResource: function() {
+
+    },
+
+    addAllResources: function() {
+
+    },
+
+    removeResource: function() {
+
+    },
+
+    clearResources: function() {
+
+    },
+
+    refresh: function() {
+
+    }
+
+  });
 
 });
