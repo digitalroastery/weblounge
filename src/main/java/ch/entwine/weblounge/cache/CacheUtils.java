57dbbda6040617a550cac05bc35575f93e74d99d
Tobias Wunden <tobias@entwinemedia.com>
2011-06-10 06:26:02 +0000

    Changed packages and configuration files to match ch.entwine [#387]

--- src/main/java/ch/entwine/weblounge/cache/CacheUtils.java
+++ src/main/java/ch/entwine/weblounge/cache/CacheUtils.java
@@ -1,7 +1,7 @@
 /*
  *  Weblounge: Web Content Management System
- *  Copyright (c) 2010 The Weblounge Team
- *  http://weblounge.o2it.ch
+ *  Copyright (c) 2003 - 2011 The Weblounge Team
+ *  http://entwinemedia.com/weblounge
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
@@ -18,10 +18,10 @@
  *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-package ch.o2it.weblounge.cache;
+package ch.entwine.weblounge.cache;
 
-import ch.o2it.weblounge.common.request.CacheTag;
-import ch.o2it.weblounge.common.site.Site;
+import ch.entwine.weblounge.common.request.CacheTag;
+import ch.entwine.weblounge.common.site.Site;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
