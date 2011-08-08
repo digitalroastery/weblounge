9dac833893da5cfdd86a907a35b10783a57240bf
Lukas Rohner <lukas@entwinemedia.com>
2011-08-08 15:38:53 +0000

    implemented login page [#517]

--- src/main/resources/login/login.js
+++ src/main/resources/login/login.js
@@ -1,10 +1,17 @@
-steal( 'resources/example' )				// Loads 'resources/example.js'
-	.css( 'login' )			// Loads 'login.css'
-	.plugins(
-		'steal/less',
-		'steal/coffee' )					// Loads 'steal/less/less.js' and 'steal/coffee/coffee.js'
-	.then(function(){						// Adds a function to be called back once all prior files have been loaded and run 
-		steal.coffee('resources/example')	// Loads 'resources/example.coffee'
-			.less('resources/example');		// Loads 'resources/example.less'
+steal.plugins('jquery/view/tmpl')
+.css('login')
+.then(function($){
+	
+	$(document).ready(function() {
+	    // Append the Weblounge Editor skeleton at the end of the page body
+		$(document.body).append('//login/views/login', {});
+		
+		
+		var test = $(document.body).find('input[name="username"]').focus();
+		if (window.location.search.substring(1) == 'error') {
+			$(document.body).find('.error').show();
+		} 
 	});
+
+});
 	 
