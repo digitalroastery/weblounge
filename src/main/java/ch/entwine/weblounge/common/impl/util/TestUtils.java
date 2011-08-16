fed7e2d0809545b4f84d4825e9172b3a78c6c7fb
Lukas Rohner <lukas@entwinemedia.com>
2011-08-16 07:51:01 +0000

    fixes on integration tests

--- src/main/java/ch/entwine/weblounge/common/impl/util/TestUtils.java
+++ src/main/java/ch/entwine/weblounge/common/impl/util/TestUtils.java
@@ -20,10 +20,6 @@
 
 package ch.entwine.weblounge.common.impl.util;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-
 import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.http.HeaderIterator;
 import org.apache.http.HttpResponse;
@@ -36,10 +32,8 @@ import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.utils.URIUtils;
 import org.apache.http.client.utils.URLEncodedUtils;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-import org.slf4j.Logger;
 import org.w3c.dom.Document;
 
 import java.io.ByteArrayInputStream;
@@ -50,7 +44,6 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
@@ -89,74 +82,6 @@ public final class TestUtils {
       throw new RuntimeException("Error reading test resource at " + path);
     }
     return template;
-  }
-  
-  /**
-   * Test for the correct response when etag header is set
-   * 
-   * @param request
-   * @param eTagValue
-   * @param logger
-   * @throws Exception
-   */
-  public static void testETagHeader(HttpUriRequest request, String eTagValue, Logger logger) throws Exception {
-    DefaultHttpClient httpClient = new DefaultHttpClient();
-    try {
-      request.removeHeaders("If-Modified-Since");
-      request.setHeader("If-None-Match", eTagValue);
-      logger.info("Sending 'If-None-Match' request to {}", request.getURI());
-      HttpResponse response = TestUtils.request(httpClient, request, null);
-      assertEquals(HttpServletResponse.SC_NOT_MODIFIED, response.getStatusLine().getStatusCode());
-      assertNull(response.getEntity());
-    } finally {
-      httpClient.getConnectionManager().shutdown();
-    }
-    
-    httpClient = new DefaultHttpClient();
-    try {
-      request.removeHeaders("If-Modified-Since");
-      request.setHeader("If-None-Match", "\"abcdefghijklmt\"");
-      logger.info("Sending 'If-None-Match' request to {}", request.getURI());
-      HttpResponse response = TestUtils.request(httpClient, request, null);
-      assertEquals(HttpServletResponse.SC_OK, response.getStatusLine().getStatusCode());
-      assertNotNull(response.getEntity());
-    } finally {
-      httpClient.getConnectionManager().shutdown();
-    }
-  }
-  
-  /**
-   * Test for the correct response when modified since header is set
-   * 
-   * @param request
-   * @param eTagValue
-   * @param logger
-   * @throws Exception
-   */
-  public static void testModifiedHeader(HttpUriRequest request, Logger logger) throws Exception {
-    DefaultHttpClient httpClient = new DefaultHttpClient();
-    try {
-      request.removeHeaders("If-None-Match");
-      request.setHeader("If-Modified-Since", "Wed, 16 Feb 2011 21:06:40 GMT");
-      logger.info("Sending 'If-Modified-Since' request to {}", request.getURI());
-      HttpResponse response = TestUtils.request(httpClient, request, null);
-      assertEquals(HttpServletResponse.SC_NOT_MODIFIED, response.getStatusLine().getStatusCode());
-      assertNull(response.getEntity());
-    } finally {
-      httpClient.getConnectionManager().shutdown();
-    }
-    
-    httpClient = new DefaultHttpClient();
-    try {
-      request.removeHeaders("If-None-Match");
-      request.setHeader("If-Modified-Since", "Wed, 10 Feb 1999 21:06:40 GMT");
-      logger.info("Sending 'If-Modified-Since' request to {}", request.getURI());
-      HttpResponse response = TestUtils.request(httpClient, request, null);
-      assertEquals(HttpServletResponse.SC_OK, response.getStatusLine().getStatusCode());
-      assertNotNull(response.getEntity());
-    } finally {
-      httpClient.getConnectionManager().shutdown();
-    }
   }
 
   /**
