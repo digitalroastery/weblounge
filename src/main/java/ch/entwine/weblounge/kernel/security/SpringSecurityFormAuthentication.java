787d04586c8cef93ca10ccc5b94101d8052c033f
Basil Brunner <basil@entwinemedia.com>
2011-08-23 15:42:06 +0000

    remove unused import statement

--- src/main/java/ch/entwine/weblounge/kernel/security/SpringSecurityFormAuthentication.java
+++ src/main/java/ch/entwine/weblounge/kernel/security/SpringSecurityFormAuthentication.java
@@ -25,7 +25,6 @@ import org.springframework.security.authentication.AuthenticationServiceExceptio
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.TextEscapeUtils;
 
