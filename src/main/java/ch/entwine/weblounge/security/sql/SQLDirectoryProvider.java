0e831cdb32ae2701b34db700bc2f7a0b4fb06d8d
Tobias Wunden <tobias@entwinemedia.com>
2011-09-17 14:11:15 +0000

    Added configuration support to system directory [#623]

--- src/main/java/ch/entwine/weblounge/security/sql/SQLDirectoryProvider.java
+++ src/main/java/ch/entwine/weblounge/security/sql/SQLDirectoryProvider.java
@@ -28,9 +28,12 @@ import ch.entwine.weblounge.common.security.Role;
 import ch.entwine.weblounge.common.security.User;
 import ch.entwine.weblounge.common.site.Site;
 
+import org.apache.commons.lang.StringUtils;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.jdbc.DataSourceFactory;
 import org.osgi.service.jpa.EntityManagerFactoryBuilder;
@@ -42,21 +45,34 @@ import java.sql.Driver;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Dictionary;
 import java.util.Properties;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
 /**
- * TODO: Comment SQLDirectoryProvider
+ * This directory provider connects to a central database that can hold users
+ * for multiple sites.
+ * <p>
+ * The database connection can be defined in
+ * <code>sqldirectory.properties</code> and should be set up using the following
+ * scheme:
+ * 
+ * <pre>
+ * TODO: Add CREATE TABLE and CREATE INDEX statements
+ * </pre>
  */
-public class SQLDirectoryProvider implements DirectoryProvider {
+public class SQLDirectoryProvider implements DirectoryProvider, ManagedService {
 
   /** The logging facility */
-  private static final Logger log = LoggerFactory.getLogger(SQLDirectoryProvider.class);
+  private static final Logger logger = LoggerFactory.getLogger(SQLDirectoryProvider.class);
 
   /** Identifier of this directory provider */
   private static final String PROVIDER_IDENTIFIER = "weblounge-sql-database";
+
+  /** Service pid, used to look up the service configuration */
+  public static final String SERVICE_PID = "ch.entwine.weblounge.sqldirectory";
 
   /** SQL query used to load a user for a specific site */
   private static final String SQL_QUERY_USER_ACCOUNT = "SELECT u.id, u.firstname, u.lastname, u.email, u.password FROM users u INNER JOIN user_accounts ua ON u.id = ua.user_id WHERE ua.enabled = true AND u.id = ? AND ua.site_id = ?";
@@ -64,7 +80,23 @@ public class SQLDirectoryProvider implements DirectoryProvider {
   /** SQL query used to load the roles of a user account */
   private static final String SQL_QUERY_UA_ROLES = "SELECT r.context, r.rolename FROM user_account_roles r INNER JOIN user_accounts ua ON r.user_account_id =  ua.id WHERE ua.user_id = ? AND  ua.site_id = ?";
 
-  private BundleContext context = null;
+  /** Configuration option for the database server and database */
+  private static final String OPT_DB_URL = "sqldirectory.url";
+
+  /** Configuration option for the database user */
+  private static final String OPT_DB_USER = "sqldirectory.user";
+
+  /** Configuration option for the database user */
+  private static final String OPT_DB_PASSWORD = "sqldirectory.password";
+
+  /** The bundle context */
+  private BundleContext bundleCtx = null;
+
+  /** The connection properties */
+  private Properties connectionProperties = null;
+
+  /** The driver configured to the directory database */
+  private Driver databaseDriver = null;
 
   /**
    * Callback to activate the component.
@@ -73,7 +105,90 @@ public class SQLDirectoryProvider implements DirectoryProvider {
    *          the declarative services component context
    */
   public void activate(ComponentContext cc) {
-    context = cc.getBundleContext();
+    bundleCtx = cc.getBundleContext();
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.osgi.service.cm.ManagedService#updated(java.util.Dictionary)
+   */
+  @SuppressWarnings("rawtypes")
+  public void updated(Dictionary properties) throws ConfigurationException {
+    if (properties == null)
+      return;
+
+    // Disconnect from the current database
+    disconnect(databaseDriver);
+    databaseDriver = null;
+    connectionProperties = null;
+
+    // Read the updated properties
+    String url = StringUtils.trimToNull((String) properties.get(OPT_DB_URL));
+    String user = StringUtils.trimToNull((String) properties.get(OPT_DB_USER));
+    String password = StringUtils.trimToNull((String) properties.get(OPT_DB_PASSWORD));
+
+    if (url == null) {
+      logger.info("Database provider is not configured");
+      return;
+    }
+
+    // Store url and properties
+    connectionProperties = new Properties();
+    connectionProperties.put(DataSourceFactory.JDBC_URL, url);
+    if (user != null)
+      connectionProperties.put(DataSourceFactory.JDBC_USER, user);
+    if (password != null)
+      connectionProperties.put(DataSourceFactory.JDBC_PASSWORD, password);
+
+    // Connect to the new database
+    databaseDriver = connect(connectionProperties);
+  }
+
+  /**
+   * Connects to the database and returns the database driver, which can be used
+   * to create individual database connections.
+   * 
+   * @param properties
+   *          the connection properties
+   * @return the database driver
+   */
+  private Driver connect(Properties properties) {
+    String filter = "(osgi.jdbc.driver.class=com.mysql.jdbc.Driver)";
+    ServiceReference[] sr;
+    DataSourceFactory dsf = null;
+    try {
+      sr = bundleCtx.getServiceReferences(DataSourceFactory.class.getName(), filter);
+      if (sr != null && sr.length > 0) {
+        dsf = (DataSourceFactory) bundleCtx.getService(sr[0]);
+      } else {
+        logger.debug("No DataSourceFactory found.");
+        return null;
+      }
+    } catch (InvalidSyntaxException e) {
+      logger.error(e.getMessage());
+      return null;
+    }
+
+    // Create the driver
+    try {
+      return dsf.createDriver(null);
+    } catch (SQLException e) {
+      logger.error(e.getMessage());
+      return null;
+    }
+  }
+
+  /**
+   * Disconnects from the datasource.
+   * 
+   * @param driver
+   *          the database driver
+   */
+  private void disconnect(Driver driver) {
+    if (driver == null)
+      return;
+    logger.info("Weblounge database provider disconnected");
   }
 
   /**
@@ -97,7 +212,7 @@ public class SQLDirectoryProvider implements DirectoryProvider {
       try {
         conn.close();
       } catch (SQLException e) {
-        log.error(e.getMessage());
+        logger.error(e.getMessage());
       }
     }
 
@@ -118,7 +233,7 @@ public class SQLDirectoryProvider implements DirectoryProvider {
         user.addPrivateCredentials(new PasswordImpl(rs.getString(5), DigestType.plain));
       }
     } catch (SQLException e) {
-      log.error(e.getMessage());
+      logger.error(e.getMessage());
       return null;
     }
     return user;
@@ -136,7 +251,7 @@ public class SQLDirectoryProvider implements DirectoryProvider {
         user.addPublicCredentials(new RoleImpl(rs.getString(1), rs.getString(2)));
       }
     } catch (SQLException e) {
-      log.error(e.getMessage());
+      logger.error(e.getMessage());
     }
   }
 
@@ -186,14 +301,14 @@ public class SQLDirectoryProvider implements DirectoryProvider {
    * @throws Exception
    *           If no service reference could be found
    */
-  private ServiceReference getEntityManagerFactoryServiceReference()
+  protected ServiceReference getEntityManagerFactoryServiceReference()
       throws Exception {
     String filter = "(" + EntityManagerFactoryBuilder.JPA_UNIT_NAME + "=security.sql)";
     ServiceReference[] sr;
     try {
-      sr = context.getServiceReferences(EntityManagerFactory.class.getName(), filter);
+      sr = bundleCtx.getServiceReferences(EntityManagerFactory.class.getName(), filter);
     } catch (InvalidSyntaxException e) {
-      log.error(e.getMessage());
+      logger.error(e.getMessage());
       throw new Exception("EntityManagerFactory service could not be fetched.");
     }
     if (sr != null && sr.length > 0)
@@ -202,9 +317,16 @@ public class SQLDirectoryProvider implements DirectoryProvider {
       throw new Exception("EntityManagerFactory service is not available.");
   }
 
-  private EntityManager getEntityManager() throws Exception {
+  /**
+   * Returns the directory's entity manager.
+   * 
+   * @return the entity manager
+   * @throws Exception
+   *           TODO: Comment
+   */
+  protected EntityManager getEntityManager() throws Exception {
     ServiceReference sr = getEntityManagerFactoryServiceReference();
-    EntityManagerFactory emf = (EntityManagerFactory) context.getService(sr);
+    EntityManagerFactory emf = (EntityManagerFactory) bundleCtx.getService(sr);
     return emf.createEntityManager();
   }
 
@@ -212,42 +334,18 @@ public class SQLDirectoryProvider implements DirectoryProvider {
    * Gets a connection from the database and returns it. If no connection is
    * available, <code>null</code> is returned instead.
    * 
-   * TODO: Add pooling
-   * TODO: Remove fixed connection pool
+   * TODO: Add pooling TODO: Remove fixed connection pool
    * 
    * @return the connection or <code>null</code> if no connection is available
    */
-  private Connection getDBConnection() {
-    String filter = "(osgi.jdbc.driver.class=com.mysql.jdbc.Driver)";
-    ServiceReference[] sr;
-    DataSourceFactory dsf = null;
-    try {
-      sr = context.getServiceReferences(DataSourceFactory.class.getName(), filter);
-      if (sr != null && sr.length > 0) {
-        dsf = (DataSourceFactory) context.getService(sr[0]);
-      } else {
-        log.debug("No DataSourceFactory found.");
-        return null;
-      }
-
-    } catch (InvalidSyntaxException e) {
-      log.error(e.getMessage());
+  protected Connection getDBConnection() {
+    if (databaseDriver == null)
       return null;
-    }
-
-    Driver driver = null;
-
-    Properties driverProps = new Properties();
-    // driverProps.put(DataSourceFactory.JDBC_URL,
-    // "jdbc:mysql:/web.cce043z6xgme.eu-west-1.rds.amazonaws.com:3306/weblounge");
-    driverProps.put(DataSourceFactory.JDBC_USER, "admin");
-    driverProps.put(DataSourceFactory.JDBC_PASSWORD, "4gewinnt");
-
+    String url = connectionProperties.getProperty(OPT_DB_URL);
     try {
-      driver = dsf.createDriver(null);
-      return driver.connect("jdbc:mysql://web.cce043z6xgme.eu-west-1.rds.amazonaws.com:3306/weblounge", driverProps);
+      return databaseDriver.connect(url, connectionProperties);
     } catch (SQLException e) {
-      log.error(e.getMessage());
+      logger.error(e.getMessage());
       return null;
     }
   }
