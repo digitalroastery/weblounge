23ad293375b4218ebdda137e8b8b3dabf7d8752a
Basil Brunner <basil.brunner@swissunihockey.ch>
2012-08-10 14:32:43 +0200

    Support for GZip compression added

--- modules/weblounge-maven-s3-deploy-plugin/src/main/java/ch/entwine/weblounge/maven/S3DeployMojo.java
+++ modules/weblounge-maven-s3-deploy-plugin/src/main/java/ch/entwine/weblounge/maven/S3DeployMojo.java
@@ -32,6 +32,7 @@ import com.amazonaws.services.s3.model.ProgressListener;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.transfer.TransferManager;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.maven.model.FileSet;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -39,9 +40,13 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.codehaus.plexus.util.FileUtils;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.zip.GZIPOutputStream;
 
 /**
  * @goal deploy
@@ -96,6 +101,13 @@ public class S3DeployMojo extends AbstractMojo {
    */
   private int valid;
 
+  /**
+   * Enable/disable GZip compression of files. Default value is 'true'.
+   * 
+   * @parameter default-value="true"
+   */
+  private boolean gzip;
+
   /*
    * (non-Javadoc)
    * 
@@ -120,6 +132,7 @@ public class S3DeployMojo extends AbstractMojo {
       for (File file : res) {
         // Make path of resource relative to resources directory
         String path = file.getPath().replace(resources.getDirectory(), "");
+        String extension = FileUtils.extension(file.getName());
 
         // Remove starting slash
         if (path.startsWith("/"))
@@ -135,12 +148,36 @@ public class S3DeployMojo extends AbstractMojo {
         // Concatenate key
         String key = keyPrefix + path;
 
+        // Delete old file version in bucket
         client.deleteObject(bucket, path);
 
         // Setup meta data
         ObjectMetadata meta = new ObjectMetadata();
         meta.setCacheControl("public, max-age=" + String.valueOf(valid * 3600));
-        meta.setContentLength(file.length());
+
+        FileInputStream fis = null;
+        GZIPOutputStream gzipos = null;
+        if (gzip && ("js".equals(extension) || "css".equals(extension))) {
+          try {
+            fis = new FileInputStream(file);
+            File gzFile = File.createTempFile(file.getName(), null);
+            gzipos = new GZIPOutputStream(new FileOutputStream(gzFile));
+            IOUtils.copy(fis, gzipos);
+            file = gzFile;
+            meta.setContentEncoding("gzip");
+            if ("js".equals(extension))
+              meta.setContentType("text/javascript");
+            if ("css".equals(extension))
+              meta.setContentType("text/css");
+          } catch (FileNotFoundException e) {
+            getLog().error(e);
+          } catch (IOException e) {
+            getLog().error(e);
+          } finally {
+            IOUtils.closeQuietly(fis);
+            IOUtils.closeQuietly(gzipos);
+          }
+        }
 
         // Create put object request
         PutObjectRequest request = new PutObjectRequest(bucket, key, file);
@@ -162,6 +199,7 @@ public class S3DeployMojo extends AbstractMojo {
    * @return
    * @throws MojoExecutionException
    */
+  @SuppressWarnings("unchecked")
   protected List<File> getResources() throws MojoExecutionException {
     File directory = new File(resources.getDirectory());
     String includes = getCommaSeparatedList(resources.getIncludes());
@@ -199,7 +237,7 @@ public class S3DeployMojo extends AbstractMojo {
     }
 
     public void progressChanged(ProgressEvent pe) {
-      if (pe.getEventCode() == pe.COMPLETED_EVENT_CODE) {
+      if (pe.getEventCode() == ProgressEvent.COMPLETED_EVENT_CODE) {
         getLog().info("Resource '" + key + "' deployed");
         client.setObjectAcl(bucket, key, CannedAccessControlList.PublicRead);
       }
