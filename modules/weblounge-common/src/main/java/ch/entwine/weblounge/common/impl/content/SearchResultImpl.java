567a861ddf01793492482328afa0055c7f20d310
Tobias Wunden <tobias@entwinemedia.com>
2012-09-16 20:42:36 +0200

    Fixed merge artifact

--- modules/weblounge-common/src/main/java/ch/entwine/weblounge/common/impl/content/SearchResultImpl.java
+++ modules/weblounge-common/src/main/java/ch/entwine/weblounge/common/impl/content/SearchResultImpl.java
@@ -201,16 +201,6 @@ public class SearchResultImpl implements SearchResult {
   }
 
   /**
-   * Sets the document count.
-   * 
-   * @param count
-   *          the number of documents in this search result
-   */
-  public void setDocumentCount(long count) {
-    this.documentCount = count;
-  }
-
-  /**
    * {@inheritDoc}
    * 
    * @see ch.entwine.weblounge.common.content.SearchResult#toXml()
