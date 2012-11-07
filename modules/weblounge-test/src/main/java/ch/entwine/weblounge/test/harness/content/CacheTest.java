7a89db5a21d6471e10ba96b17e6b9b1932f184d7
Tobias Wunden <tobias@entwinemedia.com>
2012-11-07 13:57:44 +0100

    Tags dealing with embedding of content into other pages are now contributing to determining the Last-Modified date of a response.
    Applies to #149

--- modules/weblounge-test/src/main/java/ch/entwine/weblounge/test/harness/content/CacheTest.java
+++ modules/weblounge-test/src/main/java/ch/entwine/weblounge/test/harness/content/CacheTest.java
@@ -20,6 +20,7 @@
 
 package ch.entwine.weblounge.test.harness.content;
 
+import static junit.framework.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -32,7 +33,9 @@ import ch.entwine.weblounge.common.url.UrlUtils;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -56,6 +59,12 @@ public class CacheTest extends IntegrationTestBase {
   /** The paths to test */
   private static final String requestPath = "/test/pagecontent";
 
+  /** The paths to host page */
+  private static final String hostPage = "/test/host";
+
+  /** The id of the page that is linked to by the host page */
+  private static final String linkedPageId = "4bb19980-8f98-4873-a813-a00000000002";
+
   /** The request language */
   private static final Language language = LanguageUtils.getLanguage(Locale.GERMAN);
 
@@ -72,41 +81,36 @@ public class CacheTest extends IntegrationTestBase {
    * @see ch.entwine.weblounge.testing.kernel.IntegrationTest#execute(java.lang.String)
    */
   @Override
-  public void execute(String serverUrl)
-      throws Exception {
+  public void execute(String serverUrl) throws Exception {
     logger.info("Testing if response cache is activated");
 
-    boolean responseCacheIsActivated = false;
-
+    // Test if the cache is active
+    logger.info("Sending request to determine cache status to {}", serverUrl);
     HttpGet request = new HttpGet(serverUrl);
-    HttpClient httpClient = null;
-    HttpResponse response = null;
+    request.addHeader("X-Cache-Debug", "yes");
+    String[][] params = new String[][] { {} };
+
+    // Send and the request and examine the response twice to make sure to get
+    // a cached response is available
+    boolean cacheIsActive = false;
     for (int i = 0; i < 2; i++) {
+      HttpClient httpClient = new DefaultHttpClient();
       try {
-        httpClient = new DefaultHttpClient();
-        response = TestUtils.request(httpClient, request, null);
+        HttpResponse response = TestUtils.request(httpClient, request, params);
+        cacheIsActive = response.getHeaders("X-Cache-Key").length > 0;
       } finally {
-        if (httpClient != null)
-          httpClient.getConnectionManager().shutdown();
+        httpClient.getConnectionManager().shutdown();
       }
     }
 
-    if (response == null)
-      throw new IllegalStateException();
-
-    // Test the Cache header
-    // FIXME Check if cache is enabled
-//    Header[] cacheHeaders = response.getHeaders("X-Cache-Key");
-//    responseCacheIsActivated = cacheHeaders != null && cacheHeaders.length > 0;
-    responseCacheIsActivated = true;
-
-    // Is the cache active?
-    if (!responseCacheIsActivated) {
+    if (!cacheIsActive) {
       logger.warn("Response cache is not available and won't be tested");
       return;
     }
 
+    logger.warn("Response cache is active");
     testCacheHeaders(serverUrl);
+    testInheritedModifcation(serverUrl);
   }
 
   private void testCacheHeaders(String serverUrl) throws Exception {
@@ -148,12 +152,6 @@ public class CacheTest extends IntegrationTestBase {
       // Prepare the second request
       response.getEntity().consumeContent();
       httpClient.getConnectionManager().shutdown();
-      
-      // See if the cache is online
-      if (response.getHeaders("X-Cache-Key").length == 0) {
-        logger.warn("Cache is turned off and is not tested");
-        return;
-      }
 
       // Give the cache time to persist the entry
       Thread.sleep(1000);
@@ -189,6 +187,130 @@ public class CacheTest extends IntegrationTestBase {
     } finally {
       httpClient.getConnectionManager().shutdown();
     }
+  }
+
+  private void testInheritedModifcation(String serverUrl) throws Exception {
+    logger.info("Preparing test of cache headers influenced by inherited updated content");
+
+    DateFormat df = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss 'GMT'", Locale.US);
+    df.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+    // Prepare the request
+    logger.info("Gathering original modification date");
+
+    String requestUrl = UrlUtils.concat(serverUrl, hostPage, language.getIdentifier());
+
+    logger.info("Sending request to {}", requestUrl);
+    HttpGet request = new HttpGet(requestUrl);
+    request.addHeader("X-Cache-Debug", "yes");
+    String[][] params = new String[][] { {} };
+
+    // Send and the request and examine the response. Keep the modification
+    // date.
+    HttpClient httpClient = new DefaultHttpClient();
+    try {
+      HttpResponse response = TestUtils.request(httpClient, request, params);
+
+      int statusCode = response.getStatusLine().getStatusCode();
+      boolean okOrNotModified = statusCode == HttpServletResponse.SC_OK || statusCode == HttpServletResponse.SC_NOT_MODIFIED;
+      assertTrue(okOrNotModified);
+
+      // Get the ETag header
+      assertNotNull(response.getHeaders("ETag"));
+      assertEquals(1, response.getHeaders("ETag").length);
+      String hostOrignalETag = response.getHeaders("ETag")[0].getValue();
+
+      // Get the Modified header
+      assertNotNull(response.getHeaders("Last-Modified"));
+      assertEquals(1, response.getHeaders("Last-Modified").length);
+      Date hostModified = df.parse(response.getHeaders("Last-Modified")[0].getValue());
+      response.getEntity().consumeContent();
+      httpClient.getConnectionManager().shutdown();
+
+      logger.info("Updating linked page");
+      update(serverUrl, linkedPageId);
+
+      // Give the cache time to persist the entry
+      Thread.sleep(1000);
+
+      httpClient = new DefaultHttpClient();
+
+      // Test using ETag and modified header
+      request.setHeader("If-None-Match", hostOrignalETag);
+      request.setHeader("If-Modified-Since", df.format(hostModified));
+
+      response = TestUtils.request(httpClient, request, params);
+      assertEquals(HttpServletResponse.SC_OK, response.getStatusLine().getStatusCode());
+
+      // Get the Expires header
+      assertNotNull(response.getHeaders("Expires"));
+      assertEquals(1, response.getHeaders("Expires").length);
+      // We are explicitly not checking for equality with the previously
+      // received value, since on first request, that value is not yet correct
+
+      // Get the ETag header and make sure it has been updated
+      assertNotNull(response.getHeaders("ETag"));
+      assertEquals(1, response.getHeaders("ETag").length);
+      assertFalse(hostOrignalETag.equals(response.getHeaders("ETag")[0].getValue()));
+
+      // Test the modified header and make sure it has been updated
+      assertNotNull(response.getHeaders("Last-Modified"));
+      assertEquals(1, response.getHeaders("Last-Modified").length);
+      Date newModified = df.parse(response.getHeaders("Last-Modified")[0].getValue());
+      assertTrue(hostModified.before(newModified));
+
+    } finally {
+      httpClient.getConnectionManager().shutdown();
+    }
+  }
+
+  /**
+   * Updates the page with the given identifier.
+   * 
+   * @param serverUrl
+   *          the server url
+   * @param id
+   *          the page identifier
+   * @throws Exception
+   *           if updating the page fails
+   */
+  private void update(String serverUrl, String id) throws Exception {
+    String requestUrl = UrlUtils.concat(serverUrl, "system/weblounge/pages");
+
+    // Lock the page
+    HttpPut lockPageRequest = new HttpPut(UrlUtils.concat(requestUrl, id, "lock"));
+    HttpClient httpClient = new DefaultHttpClient();
+    logger.info("Locking the page at {}", requestUrl);
+    try {
+      HttpResponse response = TestUtils.request(httpClient, lockPageRequest, null);
+      assertEquals(HttpServletResponse.SC_OK, response.getStatusLine().getStatusCode());
+    } finally {
+      httpClient.getConnectionManager().shutdown();
+    }
+
+    // Publish it
+    HttpPut publishPageRequest = new HttpPut(UrlUtils.concat(requestUrl, id, "publish"));
+    httpClient = new DefaultHttpClient();
+    String[][] params = new String[][] { { "modified", "true" } };
+    logger.info("Publishing the page at {}", requestUrl);
+    try {
+      HttpResponse response = TestUtils.request(httpClient, publishPageRequest, params);
+      assertEquals(HttpServletResponse.SC_OK, response.getStatusLine().getStatusCode());
+    } finally {
+      httpClient.getConnectionManager().shutdown();
+    }
+
+    // Unlock the page
+    HttpDelete unlockRequest = new HttpDelete(UrlUtils.concat(requestUrl, id, "lock"));
+    httpClient = new DefaultHttpClient();
+    logger.info("Unlocking the page at {}", requestUrl);
+    try {
+      HttpResponse response = TestUtils.request(httpClient, unlockRequest, null);
+      assertEquals(HttpServletResponse.SC_OK, response.getStatusLine().getStatusCode());
+    } finally {
+      httpClient.getConnectionManager().shutdown();
+    }
+
   }
 
 }
