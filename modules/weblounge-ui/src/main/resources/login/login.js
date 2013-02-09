2df6ac96dc3737a3bd8fd1f1fda8817789621e75
Simon Betschmann <simon@betschmann.ch>
2013-02-09 08:32:44 +0100

    set the focus on page startup
    - focus on the login-input on page startup
    - commented out the yet unused checkbox 'remember me'

--- modules/weblounge-ui/src/main/resources/login/login.js
+++ modules/weblounge-ui/src/main/resources/login/login.js
@@ -6,12 +6,13 @@ steal.plugins('jquery/view/tmpl')
 	    // Append the Weblounge Editor skeleton at the end of the page body
 		$(document.body).append('//login/views/login', {});
 		
+		// focus on the login-input on page startup
+		$("#j_username").focus();
 		
-		var test = $(document.body).find('input[name="username"]').focus();
+		// show error-message
 		if (window.location.search.substring(1) == 'error') {
 			$(document.body).find('.error').show();
 		} 
 	});
 
 });
-	 
