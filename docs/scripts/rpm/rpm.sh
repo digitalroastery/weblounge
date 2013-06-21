cda0ccca29bec4c1560cc839c7e5e227cc843264
Tobias Wunden <tobias@entwinemedia.com>
2013-06-21 12:14:32 +0200

    Fixed copying of the rpms to the repo

--- docs/scripts/rpm/rpm.sh
+++ docs/scripts/rpm/rpm.sh
@@ -125,15 +125,17 @@ sudo su "$RELEASE" -c "cp -r rpm/SRPMS/ /tmp/$RELEASE/"
 sudo su "$RELEASE" -c "cp -r rpm/RPMS/* /tmp/$RELEASE/"
 sudo mkdir -p /var/www/rpm-repos/$CUSTOMER/{SRPMS,RPMS}
 sudo cp -r /tmp/$RELEASE/SRPMS/* /var/www/rpm-repos/$CUSTOMER/SRPMS/
-sudo cp -r /tmp/$RELEASE/x86_64/entwine* /var/www/rpm-repos/$CUSTOMER/RPMS
-#sudo userdel -r "$RELEASE"
+sudo cp -r /tmp/$RELEASE/x86_64/* /var/www/rpm-repos/$CUSTOMER/RPMS
+
+# Remove the user and its home directory (and with that all the rpmbuild work directory)
+sudo userdel -r "$RELEASE"
 
 # Delete debuginfo packages from customer repo
 sudo rm -f /var/www/rpm-repos/$CUSTOMER/RPMS/*debuginfo*.rpm
 
-# Delete all packages but the 3 newest ones
-cd /var/www/rpm-repos/$CUSTOMER/RPMS && sudo ls -t1 | tail -n +4 | sudo xargs rm -r
-cd /var/www/rpm-repos/$CUSTOMER/SRPMS && sudo ls -t1 | tail -n +4 | sudo xargs rm -r
+# Delete all packages but the 3 newest ones (and don't care if there are less than 3)
+cd /var/www/rpm-repos/$CUSTOMER/RPMS && sudo ls -t1 | tail -n +4 | sudo xargs rm -r 2> /dev/null
+cd /var/www/rpm-repos/$CUSTOMER/SRPMS && sudo ls -t1 | tail -n +4 | sudo xargs rm -r 2> /dev/null
 
 # Update the customer repo
 sudo createrepo /var/www/rpm-repos/$CUSTOMER
