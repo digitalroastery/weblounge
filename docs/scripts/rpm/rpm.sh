6bf5109a60bde64770a4a9c3f3e636e15806d61d
Tobias Wunden <tobias@entwinemedia.com>
2013-06-21 15:26:45 +0200

    More fixes to paths

--- docs/scripts/rpm/rpm.sh
+++ docs/scripts/rpm/rpm.sh
@@ -66,7 +66,7 @@ sed -i s#CHANGE_ME_VERSION#"$VERSION"# "docs/scripts/rpm/weblounge.spec"
 sed -i s#CHANGE_ME_RELEASE#"$RELEASE"# "docs/scripts/rpm/weblounge.spec"
 
 # Time check
-echo "Time check: starting the rpm building preparations"
+echo "Starting the rpm build preparations"
 date +%H\:%M
 
 # Create a local user for this build
@@ -77,11 +77,13 @@ sudo rm -f "bin/start.sh"
 sudo rm -f "bin/start.bat"
 
 # Create the directory structure for putting together the RPMs
+echo "Preparing the build environment at /home/$RELEASE/weblounge.$RELEASE"
 sudo su - "$RELEASE" -c "rpmdev-setuptree"
 sudo su - "$RELEASE" -c "mkdir -p /home/$RELEASE/weblounge.$RELEASE"
 sudo su - "$RELEASE" -c "mkdir -p /home/$RELEASE/rpmbuild/{SOURCES,SPECS}"
 
 # Move the files in place for the creation of the RPMs
+echo "Moving the rpm contents to /home/$RELEASE/weblounge.$RELEASE"
 sudo cp -r "bin" /home/"$RELEASE"/weblounge."$RELEASE"
 sudo cp -r "docs" /home/"$RELEASE"/weblounge."$RELEASE"
 sudo cp -r "docs/scripts/rpm/contents/etc" /home/"$RELEASE"/weblounge."$RELEASE"
@@ -91,7 +93,8 @@ sudo cp -r "lib" /home/"$RELEASE"/weblounge."$RELEASE"
 # Switch to the rpm build directory
 sudo su - "$RELEASE" -c "cd ~"
 
-# Create the sources RPM
+# Create the tarball for the rpm
+echo "Creating a tarball at /home/$RELEASE/rpmbuild/SOURCES/weblounge.$RELEASE.tar.gz"
 sudo su - "$RELEASE" -c "tar -cvzf rpmbuild/SOURCES/weblounge.$RELEASE.tar.gz weblounge.$RELEASE"
 sudo chown "$RELEASE" /home/"$RELEASE"/rpmbuild/SOURCES/weblounge."$RELEASE".tar.gz
 
@@ -99,8 +102,8 @@ sudo chown "$RELEASE" /home/"$RELEASE"/rpmbuild/SOURCES/weblounge."$RELEASE".tar
 sudo cp "docs/scripts/rpm/weblounge.spec" /home/"$RELEASE"/rpmbuild/SPECS
 sudo chown $RELEASE /home/"$RELEASE"/rpmbuild/SPECS/weblounge.spec
 
-# Time check
-echo "Time check: end of rpm preparations, starting the rpm build process"
+# Create the rpm
+echo "Starting the rpm build process"
 date +%H\:%M
 
 sudo su - "$RELEASE" -c "rpmbuild -ba rpmbuild/SPECS/weblounge.spec"
@@ -111,24 +114,24 @@ if [ ! $rpm_exit_code -eq 0 ];then
   exit 1
 fi
 
-
-# Rime check
-echo "tTime check: package created repository operations starting"
+# Move the rpm to the repository
+echo "Moving the rpm to the rpm repository"
 date +%H\:%M
 
-# Move the package to the repo (on Entwine infrastructure)
 sudo chmod 777 -R /home/"$RELEASE"/rpmbuild
 sudo rm -rf /tmp/"$RELEASE"
 mkdir /tmp/"$RELEASE"
 chmod -R 777 /tmp/"$RELEASE"
-sudo su "$RELEASE" -c "cp -r rpmbuild/SRPMS /tmp/$RELEASE/"
-sudo su "$RELEASE" -c "cp -r rpmbuild/RPMS /tmp/$RELEASE/"
+sudo su "$RELEASE" -c "cp -r /home/$RELEASE/rpmbuild/SRPMS /tmp/$RELEASE/"
+sudo su "$RELEASE" -c "cp -r /home/$RELEASE/rpmbuild/RPMS /tmp/$RELEASE/"
 sudo mkdir -p /var/www/rpm-repos/$CUSTOMER/{SRPMS,RPMS}
-sudo cp -r /tmp/$RELEASE/SRPMS/* /var/www/rpm-repos/$CUSTOMER/SRPMS/
-sudo cp -r /tmp/$RELEASE/x86_64/* /var/www/rpm-repos/$CUSTOMER/RPMS
+
+# Move the packages to the repository
+sudo cp -r /tmp/"$RELEASE"/SRPMS/* /var/www/rpm-repos/$CUSTOMER/SRPMS/
+sudo cp -r /tmp/"$RELEASE"/RPMS/x86_64/* /var/www/rpm-repos/$CUSTOMER/RPMS
 
 # Remove the user and its home directory (and with that all the rpmbuild work directory)
-#sudo userdel -r "$RELEASE"
+sudo userdel -r "$RELEASE"
 
 # Delete debuginfo packages from customer repo
 sudo rm -f /var/www/rpm-repos/$CUSTOMER/RPMS/*debuginfo*.rpm
